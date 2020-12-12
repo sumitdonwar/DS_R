58b7e3f7d5e30884ceee251c5c5e3b41ca1aa588
sumitdonwar <73605196+sumitdonwar@users.noreply.github.com>
2020-12-12 15:27:05 +0530

    Add files via upload
    Practice commands

---
+++ Rintro101.R
@@ -0,0 +1,92 @@
+2+2
+3>5
+var<-4
+var
+
+var <- 5
+var
+3*5+6
+a<-3
+a
+a
+a<-7
+a
+2*pi*6378
+pi
+x <- 52
+x
+X<-78
+y<-50
+x+y
+a<=x+y
+a<-x+y
+a
+exp(1)
+q<-sqrt(10)
+q
+3^2
+2+3*4
+a<-5
+b<-6
+c<-a-2+b-1
+c
+>x<- 1
+X*Y*Z
+This.Year<-2020
+This.Year
+x <- 1,2,3,4 
+x<-1,2,3
+vect <- c(1,2,3,4)
+vect
+vect_char <- c("R","python","java")
+vect_char
+vec_mix <- c("R","python",10,5)
+vec_mix
+vec_mix <- c(4,2,"Python")
+vec_mix
+v <- c(4,4.2)
+v
+v[1]
+v[2]
+vect[2]<-40
+v[2]
+v
+v
+v <- c(1,2,3,4)
+v
+v[2]
+v[2]<-40
+v
+v[2]
+
+v1 <- c(10,20,30,40,50,60)
+v2<- c(11,22,33,44,55,66)
+v <- v1+v2
+v
+v1 <- c(v)
+v1
+print("hello world")
+getwd()
+setwd()
+setwd("C:/Users/sumit/Documents/R")
+getwd
+getwd()
+range <- 1:50
+range
+df <- data.frame(x=1:3, y=c("a","b","c"))
+df
+nrow(df)
+ncol(df)
+df[1,1]
+df[1,1][1,2]
+df[(1,1),(1,2)]
+df[1,y=1:2]
+df[1,range<-1:2]
+df[1,2][3,3]
+df[x=1,2 y=1,3]
+df[1,c(1,2)]
+df[c(1,3),2]
+df[c(1,3),1]
+df[c(1,3),c(1,2)]
+df[c(1,3),]  #alternative
+
